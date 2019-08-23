6f39a86289e8915517aa32ec98500710f5ad4f6a
Olybear9 <olybear9@gmail.com>
2019-08-23 10:31:33 -0600

    Javascript Map
    == Map ==
    - Add js map concept

---
+++ src/Bavfalcon9/Mavoric/misc/Map.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Javascript map concept in php
+ */
+
+class Map {
+    private $data = [];
+
+    public function __construct(Array $data=[]) {
+        $this->data = $data;
+    }
+
+    public function set(String $key, $value) : boolean {
+        $data[$key] = $value;
+        return true;
+    }
+
+    public function delete(String $key) {
+        if (!$this->get($key)) return false;
+    }
+
+    public function get(String $key) {
+        if (!isset($data[$key])) return false;
+        else return $data[$key];
+    }
+
+    public function getKeys() : Array {
+        return array_keys($data);
+    }
+
+    public function getEntries() : Array {
+        $entries = [];
+        foreach ($this->data as $k=>$v) {
+            array_push($v);
+        }
+        return $entries;
+    }
+
+    public function forEach(Function $func) {
+        return foreach($this->data as $k=>$v) {
+            $func($k, $v, array_search($k, $this->data));
+        }
+    }
+}
