4e455e95ea44654438678c14b16cdf3e09463d40
Olybear9 <olybear9@gmail.com>
2020-03-21 02:29:59 -0600

    [CHORE] Cleanup
    remove this garbage

--- src/Bavfalcon9/Mavoric/Core/Miscellaneous/Settings.php
+++ src/Bavfalcon9/Mavoric/Core/Miscellaneous/Settings.php
@@ -29,37 +29,6 @@ class Settings {
         $this->config = $config;
     }
 
-    public function getSuppressedCheats(): Array {
-        $config = $this->config->get('Suppression');
-        $all = $this->config->getNested('Suppression.all-cheats');
-        $suppressed = $this->config->getNested('Suppression.cheats');
-
-        if (!$config || $all === true) {
-            return array_keys(Mavoric::CHEATS);
-        }
-
-        if ($suppressed && !$all) {
-            return $suppressed;
-        }
-    }
-
-    public function isSuppressed(String $cheat): Bool {
-        if (!isset(Mavoric::CHEATS[$cheat])) {
-            return false;
-        }
-
-        return in_array($cheat, $this->getSuppressedCheats());
-    }
-
-    public function isEnabled(String $type): ?Bool {
-        return $this->config->getNested("$type.enabled");
-    }
-
-    public function isCheatEnabled(String $cheat): Bool {
-        if (!$this->config->getNested('Cheats.disabled')) return true;
-        return !in_array($cheat, $this->config->getNested('Cheats.disabled'));
-    }
-    
     public function getConfig(): Config {
         return $this->config;
     }
