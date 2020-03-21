1db014f0323ab45e6e0ffe6c3e5ae168af07aeda
Olybear9 <olybear9@gmail.com>
2020-03-20 22:35:46 -0600

    [PREF] Disable suppression on dev builds

--- src/Bavfalcon9/Mavoric/Events/MavoricEvent.php
+++ src/Bavfalcon9/Mavoric/Events/MavoricEvent.php
@@ -51,9 +51,12 @@ class MavoricEvent {
             return false;
         }
         
+        // Disable suppression on dev versions
+        return false;
+        /*
         $this->eventData->setCancelled($val);
         $this->isCancelled = $val;
-        return $val;
+        return $val;*/
     }
 
     /**
